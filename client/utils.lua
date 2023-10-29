df2cf70ca0b247b7f323fc5cd78bd2640c9a52d5
vipex <101529155+vipexv@users.noreply.github.com>
2023-10-30 00:43:53 +0100

    Update utils.lua

--- client/utils.lua
+++ client/utils.lua
@@ -7,7 +7,62 @@ function UIMessage(action, data)
   })
 end
 
+---@class ZoneInfo
+---@field blip table
+---@field cancelDistance number
+---@field cooldown integer
+---@field drawText table
+---@field hacking boolean
+---@field interactButton integer
+---@field interactDistance number
+---@field label string
+---@field marker table
+---@field name string
+---@field requireGun boolean
+---@field requiredPolice integer
+---@field rewardAmount table
+---@field timeToRob integer
+---@field vec3 vector3
+---@field viewDistance number
+
+-- function ShowHelpText(coords, text)
+--   AddTextEntry('HelpText', text)
+--   SetFloatingHelpTextWorldPosition(1, coords)
+--   SetFloatingHelpTextStyle(1, 1, 2, -1, 3, 0)
+--   BeginTextCommandDisplayHelp('HelpText')
+--   EndTextCommandDisplayHelp(2, false, false, -1)
+-- end
+
+function ShowFloatingText(coords, msg)
+  AddTextEntry('floatingTextNotification', msg)
+  SetFloatingHelpTextWorldPosition(1, coords)
+  SetFloatingHelpTextStyle(1, 1, 2, -1, 3, 0)
+  BeginTextCommandDisplayHelp('floatingTextNotification')
+  EndTextCommandDisplayHelp(2, false, false, -1)
+end
+
 local currentResourceName = GetCurrentResourceName()
+
+function AddBlip(coords, text, sprite, colour, scale, removeBlip)
+  local blip = AddBlipForCoord(coords.x, coords.y, coords.z)
+  SetBlipSprite(blip, sprite)
+  SetBlipColour(blip, colour)
+  SetBlipDisplay(blip, 2)
+  SetBlipAlpha(blip, 255)
+  SetBlipScale(blip, scale)
+  SetBlipAsShortRange(blip, false)
+  PulseBlip(blip)
+  BeginTextCommandSetBlipName("STRING")
+  AddTextComponentString(text)
+  EndTextCommandSetBlipName(blip)
+  if removeBlip then
+    SetTimeout(600 * 1000, function()
+      RemoveBlip(blip)
+      print(("(DEBUG) Blip removed after 600 seconds since the removeBlip boolean was set to true once the AddBlip function was called."))
+    end)
+  end
+  return blip
+end
 
 local debugIsEnabled = GetConvarInt(('%s-debugMode'):format(currentResourceName), 0) == 1
 
