ecc3e8bac2dbd818f6ca5f024fc3055cbecbf620
vipex <101529155+vipexv@users.noreply.github.com>
2023-12-06 01:16:50 +0100

    Update

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -5,8 +5,6 @@ import { fetchNui } from "./utils/fetchNui";
 import { useNuiEvent } from "./hooks/useNuiEvent";
 import { isEnvBrowser } from "./utils/misc";
 
-// This will set the NUI to visible if we are
-// developing in browser
 debugData([
   {
     action: "setVisible",
@@ -14,29 +12,7 @@ debugData([
   },
 ]);
 
-interface ReturnClientDataCompProps {
-  data: any;
-}
-
-const ReturnClientDataComp: React.FC<ReturnClientDataCompProps> = ({
-  data,
-}) => (
-  <>
-    <h5>Returned Data:</h5>
-    <pre>
-      <code>{JSON.stringify(data, null)}</code>
-    </pre>
-  </>
-);
-
-interface ReturnData {
-  x: number;
-  y: number;
-  z: number;
-}
-
 const App: React.FC = () => {
-  const [clientData, setClientData] = useState<ReturnData | null>(null);
   const [visible, setVisible] = useState(false);
 
   useNuiEvent<boolean>("setVisible", setVisible);
@@ -56,26 +32,9 @@ const App: React.FC = () => {
     return () => window.removeEventListener("keydown", keyHandler);
   }, [visible]);
 
-  const handleGetClientData = () => {
-    fetchNui<ReturnData>("getClientData")
-      .then((retData) => {
-        console.log("Got return data from client scripts:");
-        console.dir(retData);
-        setClientData(retData);
-      })
-      .catch((e) => {
-        console.error("Setting mock data due to error", e);
-        setClientData({ x: 500, y: 300, z: 200 });
-      });
-  };
-
   return (
-    <div className=" absolute top-2 right-2 font-main text-xl text-blue-600 p-2 rounded text-center">
-      <h1 className="rounded p-2 uppercase">
-        <i className="fa-solid fa-id-card-clip"></i> Cash/Bank
-      </h1>
-      <p>$ 10000</p>
-      <p className="text-green-600">$ 10000 </p>
+    <div className="absolute top-2 right-2 font-main text-xl p-2 rounded text-center">
+      Example.
     </div>
   );
 };
