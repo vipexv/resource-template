1fe02b6c4b22a42b67c58d9503b68c6077321e5b
vipex <101529155+vipexv@users.noreply.github.com>
2023-12-21 21:50:55 +0100

    Fix

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -7,5 +7,12 @@ export default defineConfig({
   base: "./",
   build: {
     outDir: "dist",
+    chunkSizeWarningLimit: 100,
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === "MODULE_LEVEL_DIRECTIVE") return;
+        warn(warning);
+      },
+    },
   },
 });
