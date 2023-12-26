a08607cc46d8d1b23b8c4cf366171ca4c1701fb9
vipex <101529155+vipexv@users.noreply.github.com>
2023-12-26 03:56:51 +0100

    refactor: add more debug statements

--- client/events.lua
+++ client/events.lua
@@ -1,3 +1,4 @@
 RegisterNetEvent("UIMessage", function(action, data)
     UIMessage(action, data)
+    Debug("(netEvent) [UIMessage] Action: ", json.encode(action), "Data: ", json.encode(data))
 end)
