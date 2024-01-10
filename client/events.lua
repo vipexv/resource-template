2060ace29711293f2069e9c5e766eb62f26f4175
vipex <101529155+vipexv@users.noreply.github.com>
2024-01-11 00:08:42 +0100

    perf: add million.js to improve performance

--- client/events.lua
+++ client/events.lua
@@ -1,5 +1,6 @@
 RegisterNetEvent("UIMessage", function(action, data)
     UIMessage(action, data)
+
     Debug("(netEvent) [UIMessage] \n (param) action: ", json.encode(action), "\n (param) data: ", json.encode(data),
         "\n Invoking Resource: ",
         GetInvokingResource())
