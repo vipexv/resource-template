86d54efe6adb67596e0972585bebbdcd3180e115
vipex <101529155+vipexv@users.noreply.github.com>
2024-01-11 00:18:36 +0100

    refactor: change show-nui command name to include resource name at the end

--- client/commands.lua
+++ client/commands.lua
@@ -1,4 +1,4 @@
-RegisterCommand('show-nui', function()
+RegisterCommand(("show-nui-%s"):format(GetCurrentResourceName()), function()
     ToggleNuiFrame(true)
     Debug("[command:show-nui] ToggleNuiFrame called and set to true.")
 end, false)
