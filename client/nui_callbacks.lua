20c6190158a1038e27a5a14e982bb568db87a0cc
vipex <101529155+vipexv@users.noreply.github.com>
2024-02-07 22:45:02 +0100

    refactor: switch to RegisterNuiCallback func

--- client/nui_callbacks.lua
+++ client/nui_callbacks.lua
@@ -1,11 +1,11 @@
-RegisterNUICallback('hideFrame', function(_, cb)
+RegisterNuiCallback('hideFrame', function(_, cb)
     ToggleNuiFrame(false)
     Debug('[nuicb:hideFrame] called')
     cb({})
 end)
 
 -- ex.
-RegisterNUICallback('getClientData', function(data, cb)
+RegisterNuiCallback('getClientData', function(data, cb)
     local curCoords = GetEntityCoords(PlayerPedId())
 
     local retData <const> = { x = curCoords.x, y = curCoords.y, z = curCoords.z }
