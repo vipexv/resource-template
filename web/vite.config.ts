d20d3ba39b11d606ecdde4074893e61f7abfc46b
vipex <101529155+vipexv@users.noreply.github.com>
2024-06-02 17:57:50 +0200

    dev: update million, million lint

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,10 +1,11 @@
 import million from 'million/compiler';
+import MillionLint from '@million/lint';
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [million.vite({ auto: true }), react()],
+  plugins: [million.vite({ auto: true }), MillionLint.vite(), react()],
   base: "./",
   build: {
     outDir: "dist",
